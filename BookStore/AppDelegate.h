c538b2b0504860989dc43cfd50f29285a5b3aefc
caoyudong <664521714@qq.com>
2014-11-21 09:43:57 +0800

    Initial Commit

---
+++ BookStore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookStore
+//
+//  Created by cyd on 14-11-21.
+//  Copyright (c) 2014年 cyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
